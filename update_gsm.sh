00afb1c0eadcc2edc3e78807ef1ef571e9fe73f4
oitds <ashimqwerty125@gmail.com>
2025-12-03 14:57:15 +0530

    Update update_gsm.sh

--- update_gsm.sh
+++ update_gsm.sh
@@ -7,5 +7,5 @@ sed -i 's/^#0,154/0,154/' "$FILE"
 
 # Activate alt server port
 sed -i 's/^#0,155/0,155/' "$FILE"
-
+chmod +x /home/root/mdt/CoreMDT
 echo "************prop.txt updated successfully.please reboot device******************"
